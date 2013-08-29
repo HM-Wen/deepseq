72b9422d8b5b2a55ebc63d820dc9855f678df82f
Vipin <vipin at cbio.mskcc.org>
2013-08-29 19:10:09 -0400

    Galaxy wrapper for DESeq2 program

---
+++ difftest/DESeq2-hts/bin/deseq2_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DESEQ2_VERSION=1.0.19
+export DESEQ2_PATH=
+export DESEQ2_SRC_PATH=$DESEQ2_PATH/src
+export DESEQ2_BIN_PATH=$DESEQ2_PATH/bin
+export INTERPRETER=
+export MATLAB_BIN_PATH=
+export MATLAB_MEX_PATH=
+export MATLAB_INCLUDE_DIR=
+export OCTAVE_BIN_PATH=
+export OCTAVE_MKOCT=
+export SAMTOOLS_DIR=
+export PYTHON_PATH=
+export SCIPY_PATH=
+export R_PATH=
+export LD_LIBRARY_PATH=
+export ENVIRONMENT=galaxy
