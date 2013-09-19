c08e941ab4ed187ac0028c17643de559c597343f
Vipin <vipin at cbio.mskcc.org>
2013-09-19 16:35:02 -0400

    environment path config file

---
+++ difftest/DEXSeq/bin/dexseq_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+export DEXSEQ_VERSION=1.6.0
+export SAMTOOLS_DIR=/home/vipin/apps/gx-tools/samtools
+export PYTHON_PATH=/usr/bin/python
+export PYTHONPATH=/home/vipin/HomePath/lib/python2.7/site-packages/:$PYTHONPATH
+export R_PATH=/home/vipin/HomePath/bin/R
