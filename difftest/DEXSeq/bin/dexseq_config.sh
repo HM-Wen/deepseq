ae4bea06fe3b56f6992cb4151e2854ebb2f89197
Vipin <vipin at cbio.mskcc.org>
2013-09-19 17:14:19 -0400

    cleaned the config file

--- difftest/DEXSeq/bin/dexseq_config.sh
+++ difftest/DEXSeq/bin/dexseq_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 export DEXSEQ_VERSION=1.6.0
-export SAMTOOLS_DIR=/home/vipin/apps/gx-tools/samtools
+export SAMTOOLS_DIR=
 export PYTHON_PATH=/usr/bin/python
-export PYTHONPATH=/home/vipin/HomePath/lib/python2.7/site-packages/:$PYTHONPATH
-export R_PATH=/home/vipin/HomePath/bin/R
+export PYTHONPATH=
+export R_PATH=
